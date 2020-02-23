4f882a9cc06a5914fa25646d82412b3d232f0e99
João David <joaodavid1999@hotmail.com>
2020-02-23 19:11:30 +0000

    Add getters and setters to TournamentDto

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/dto/TournamentDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/dto/TournamentDto.java
@@ -2,6 +2,7 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,4 +13,38 @@ public class TournamentDto {
     private Set<Topic> topics = new HashSet<>();
 
     public TournamentDto() {}
+
+    public String getAvailableDate() { return availableDate; }
+
+    public void setAvailableDate(String availableDate) {
+        this.availableDate = availableDate;
+    }
+
+    public String getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(String startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public int getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public void setNumberOfQuestions(int numberOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+    }
+
+    public Set<Topic> getTopics() {
+        return topics;
+    }
+
+    public void addTopics(Collection<Topic> topics) {
+        this.topics.addAll(topics);
+    }
+
+    public void addTopic(Topic topic) {
+        this.topics.add(topic);
+    }
 }
