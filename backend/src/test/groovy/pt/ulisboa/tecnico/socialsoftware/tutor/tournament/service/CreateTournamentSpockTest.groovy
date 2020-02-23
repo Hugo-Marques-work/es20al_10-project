a4bab92e2f8d52304541dbd1ef3ff583bd52df21
João David <joaodavid1999@hotmail.com>
2020-02-23 17:07:44 +0000

    Define test cases

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CreateTournamentSpockTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CreateTournamentSpockTest.groovy
@@ -4,4 +4,38 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
 import spock.lang.Specification
 
 class CreateTournamentSpockTest extends Specification {
+
+    def tournamentService
+
+    def setup() {
+        tournamentService = new TournamentService()
+    }
+
+    def "topic list is null"() {
+        expect: false
+    }
+
+    def "topic list is empty"() {
+        expect: false
+    }
+
+    def "topic list isn't empty but is invalid"() {
+        expect: false
+    }
+
+    def "starting date is empty"() {
+        expect: false
+    }
+
+    def "conclusion date is emtpy"() {
+        expect: false
+    }
+
+    def "dates overlap"() {
+        expect: false
+    }
+
+    def "number of questions lesser than 0"() {
+        expect: false
+    }
 }
