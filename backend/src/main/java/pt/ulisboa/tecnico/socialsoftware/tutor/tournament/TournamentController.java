26cb1f6e8f406ba731faa90e502a75e8eb59ea00
matosberna <matosberna@hotmail.com>
2020-05-05 00:02:52 +0100

    Add get quiz of tournament to controler and jmeter test #138

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -6,6 +6,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.dto.QuizDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.statement.dto.StatementQuizDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
 
@@ -61,6 +63,15 @@ public class TournamentController {
     public ResponseEntity cancelTournament(@PathVariable int tournamentId) {
         tournamentService.cancelTournament(tournamentId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/tournaments/{tournamentId}/quiz")
+    @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#tournamentId, 'TOURNAMENT.ACCESS')")
+    public StatementQuizDto getAvailableQuiz(Principal principal, @PathVariable int tournamentId) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+        checkUserAuth(user);
+
+        return tournamentService.getAvailableQuiz(user.getId(), tournamentId);
     }
 
     private void checkUserAuth(User user) {
